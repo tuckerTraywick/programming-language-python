a49330f7efb11f3002725788cef65bfddc4aa320
Tucker Traywick <tucker.traywick@gmail.com>
2025-05-05 21:12:30 -0400

    Started writing grammar.

--- source/main.py
+++ source/main.py
@@ -2,7 +2,7 @@ from lexer import *
 from parser import *
 
 if __name__ == "__main__":
-	text = "x as int32->f()"
+	text = "x as int32()"
 	(tokens, lexingErrors) = lex(text)
 	print("---- TOKENS ----")
 	print("\n".join(map(str, tokens)))
